51858fb8ff804d2e8b0da9aaf1ea319ea64b8a3d
Sarthakmp007 <Sarthakmp007@gmail.com>
2025-06-19 02:30:30 +0530

    update 4

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,14 +20,16 @@ namespace MordernClock
         
         TranslateTransform[] gridTransforms = new TranslateTransform[3];
         double[] currPositions = new double[3];
+        int offset = 50;
         public MainWindow()
         {
             
             InitializeComponent();
             DataContext = this;
             GENERATEGRIDCOLUMNS();
-            
-            
+            OnMainGrid();
+
+
 
         }
         
@@ -39,38 +41,27 @@ namespace MordernClock
 
             if (grid_index < 0 || grid_index > gridTransforms.Length) return;
             DoubleAnimation animation = new DoubleAnimation();
-            animation.From = currPositions[grid_index];
-            animation.To = currPositions[grid_index];
             animation.Duration = new Duration(TimeSpan.FromSeconds(1));
             switch (e.Key)
             {
                 case Key.W:
                     {
-                        if (grid_index <= 2)
-                        {
-                            animation.From = currPositions[grid_index];
-                            currPositions[grid_index] += 50;
-                            animation.To = currPositions[grid_index];
+                        
+                       animation.From = currPositions[grid_index];
+                       currPositions[grid_index] += offset;
+                       animation.To = currPositions[grid_index];
+
+                       gridTransforms[grid_index].BeginAnimation(TranslateTransform.YProperty, animation);
                             
-                            gridTransforms[grid_index].BeginAnimation(TranslateTransform.YProperty, animation);
-                            break;
-                        }
-                        else
-                        {
-                            animation.From = currPositions[grid_index];
-                            currPositions[grid_index] += 15;
-                            animation.To = currPositions[grid_index];
-                            
-                            gridTransforms[grid_index].BeginAnimation(TranslateTransform.YProperty, animation);
-                            break;
-                        }
-                        }
+                       
+                        break;
+                    }
                 case Key.S:
                     {
                         
                         
                         animation.From = currPositions[grid_index];
-                        currPositions[grid_index] -= 50;
+                        currPositions[grid_index] -= offset;
                         animation.To = currPositions[grid_index];
                         gridTransforms[grid_index].BeginAnimation(TranslateTransform.YProperty, animation);
                         break;
@@ -79,187 +70,198 @@ namespace MordernClock
             }
             
         }
+
+        void Oscilation(UserControl user, int row)
+        {
+            int count = 0;
+            TranslateTransform translateTransform = new TranslateTransform();
+            user.RenderTransform = translateTransform;
+
+            double up = Math.Sin(count * (Math.PI / ( row)));
+            double from = up>0 ? 0 : offset * row;
+            double to = up<0 ? -offset * row :0;
+
+            DoubleAnimation animation = new DoubleAnimation
+            {
+                From = from,
+                To = to,
+                Duration = new Duration(TimeSpan.FromSeconds(1)),
+                AutoReverse = true,
+                RepeatBehavior = RepeatBehavior.Forever
+            };
+                
+            
+            translateTransform.BeginAnimation(TranslateTransform.YProperty, animation);
+            count++;
+
+        }
+
         Grid[] Grids = new Grid[3];
-        Grid[] ParentGrids = new Grid[8];
+        Grid[] ParentGrids = new Grid[3];
         ColumnDefinition[] columns = new ColumnDefinition[8];
-        
+
         void GENERATEGRIDCOLUMNS()
         {
-
-            //Generates 3 grid 1:0-2 2:0-5 3:0-9 (i)
-            for (int i = 1; i <= 8; i++)
+            for (int i =0; i<8; i++)
             {
-
-                ParentGrids[i - 1] = new Grid();
-
-                columns[i-1] = new ColumnDefinition();
-                if (i % 3 == 0)
+                if ((i+1) % 3 != 0)
                 {
-                    columns[i - 1].Width = new GridLength(0.6f, GridUnitType.Star);
-                    Main.ColumnDefinitions.Add(columns[i - 1]);
+                    columns[i] = new ColumnDefinition
+                    {
+                        Width = new GridLength(0.5, GridUnitType.Star)
+                    };
+                    Main.ColumnDefinitions.Add(columns[i]);
                 }
                 else
                 {
-
-                    columns[i - 1].Width = new GridLength(1f, GridUnitType.Star);
-                    Main.ColumnDefinitions.Add(columns[i - 1]);
+                    columns[i] = new ColumnDefinition
+                    {
+                        Width = new GridLength(1, GridUnitType.Star)
+                    };
+                    Main.ColumnDefinitions.Add(columns[i]);
                 }
-
-
-                if (i <= 3)
-                {
-                    Grids[i-1] = new Grid();
-
-                    Border OuterBorder = new Border
-                    {
-                        Height = 150 * i,
-                        Width = 50,
-                        //Style = (Style)FindResource("ShadowBox"),
-                        CornerRadius = new CornerRadius(10),
-                    };
-
-                    Border InnerBorder = new Border
-                    {
-                        Height = 150 * i,
-                        Width = 50,
-                        Style = (Style)FindResource("ShadowBox"),
-                        CornerRadius = new CornerRadius(10),
-                    };
-
-                    Border Emmission = new Border
-                    {
-                        Height = 150 * i,
-                        Width = 50,
-                        Style = (Style)FindResource("Emission"),
-                        CornerRadius = new CornerRadius(10),
-                    };
-                    Emmission.Effect = new DropShadowEffect
-                    {
-                        Color = Colors.White,
-                        ShadowDepth = 5,
-                        BlurRadius = 8,
-                        Opacity = 0.8,
-                        Direction=145,
-                    };
-
-                    Rectangle rect = new Rectangle
-                    {
-                        Height = 150 * i,
-                        Fill = new SolidColorBrush(Color),
-                        RadiusX = 10,
-                        RadiusY = 10
-                    };
-                    
-                    Emmission.Child = OuterBorder;
-                    OuterBorder.Child = InnerBorder;
-                    InnerBorder.Child = rect;
-                    
-
-                    rect.Width = Grids[i - 1].Width;
-                    rect.Height = Grids[i - 1].Height;
-                    for (int j = 1; j<=i*3; j++)
-                    {
-                        Grids[i-1].Height = 150*i;
-                        Grids[i - 1].Width = 50;
-
-                        if (i != 3)
-                        {
-                            Grids[i - 1].RowDefinitions.Add(new RowDefinition
-                            {
-                                Height = new GridLength(1f, GridUnitType.Star)
-                            });
-                            TextBlock text = new TextBlock
-                            {
-                                Text = $"{j - 1}",
-                                Foreground = Brushes.White,
-                                HorizontalAlignment = HorizontalAlignment.Center,
-                                VerticalAlignment = VerticalAlignment.Center
-                            };
-                            text.Effect = new DropShadowEffect {
-                                Color = Colors.White,
-                                ShadowDepth = 1,
-                                BlurRadius = 20,
-                                Opacity = 1,
-                            };
-
-
-
-                            Grid.SetRow(text, j-1);
-                            Grids[i-1].Children.Add(text);
-                           
-
-                        }
-                        else
-                        {
-                            for (int k = 0; k <= 9; k++)
-                            {
-                                Grids[i - 1].RowDefinitions.Add(new RowDefinition
-                                {
-                                    Height = new GridLength(1f, GridUnitType.Star)
-                                });
-                                TextBlock text = new TextBlock
-                                {
-                                    Text = $"{k}",
-                                    Foreground = Brushes.White,
-                                    HorizontalAlignment = HorizontalAlignment.Center,
-                                    VerticalAlignment = VerticalAlignment.Center
-                                };
-                                text.Effect = new DropShadowEffect
-                                {
-                                    Color = Colors.White,
-                                    ShadowDepth = 1,
-                                    BlurRadius = 20,
-                                    Opacity = 1,
-                                    
-                                };
-
-                                Grid.SetRow(text, k);
-                                Grids[i - 1].Children.Add(text);
-                            }
-                            //ParentGrids[i - 1].Children.Add(Grids[i - 1]);
-                            break;
-                        }
-
-                    }
-                    ParentGrids[i - 1].Children.Add(Emmission);
-                    ParentGrids[i - 1].Height = 150 * i;
-                    ParentGrids[i - 1].Width = 50;
-                   
-                    ParentGrids[i - 1].Children.Add(Grids[i - 1]);
-                    if (i == 2 || i ==3)
-                    {
-                        ParentGrids[i - 1].Margin = new Thickness(0, 0, 0,50);
-                    }
-                    TranslateTransform indivisualTransform = new TranslateTransform();
-                    gridTransforms[i - 1] = indivisualTransform;
-                    currPositions[i - 1] = 0;
-                    ParentGrids[i - 1].RenderTransform = indivisualTransform;
-
-                }
-
                 
 
-
             }
-            
 
+        }
 
-           
-            for (int i =0; i <3; i++)
+        UserControl GenGrid(int grid_index,int row)
+        {
+            Grids[grid_index] = new Grid
             {
+                Width = 50,
+                Height = offset * row,
+                
+            };
+            ParentGrids[grid_index] = new Grid();
 
-                Grid.SetColumn(ParentGrids[i], i);
-                Main.Children.Add(ParentGrids[i]);
+            Border OuterBorder = new Border
+            {
+                Width = 50,
+                
+                CornerRadius = new CornerRadius(10),
+            };
+
+            Border InnerBorder = new Border
+            {
+                Width = 50,
+                Style = (Style)FindResource("ShadowBox"),
+                CornerRadius = new CornerRadius(10),
+            };
+
+            Border Emmission = new Border
+            {
+                Width = 50,
+                Style = (Style)FindResource("Emission"),
+                CornerRadius = new CornerRadius(10),
+            };
+            Emmission.Effect = new DropShadowEffect
+            {
+                Color = Colors.White,
+                ShadowDepth = 5,
+                BlurRadius = 8,
+                Opacity = 0.8,
+                Direction = 145,
+            };
+
+            Rectangle rect = new Rectangle
+            {
+                Fill = new SolidColorBrush(Color),
+                RadiusX = 10,
+                RadiusY = 10,
+            };
+
+            Emmission.Child = OuterBorder;
+            OuterBorder.Child = InnerBorder;
+            InnerBorder.Child = rect;
+
+            
+            rect.Width = Grids[grid_index].Width;
+            rect.Height = Grids[grid_index].Height;
+
+
+            for (int i =0; i<row; i++)
+            {
+                Grids[grid_index].RowDefinitions.Add(new RowDefinition
+                {
+                    Height = new GridLength(1, GridUnitType.Star)
+                });
+
+                TextBlock textBlock = new TextBlock
+                {
+                    Text = $"{i}",
+                    FontSize = 20,
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    Foreground = Brushes.White,
+                    Effect = new DropShadowEffect
+                    {
+                        Color = Colors.Wheat,
+                        ShadowDepth = 1,
+                        BlurRadius = 2,
+                        Opacity = 0.5,
+                        Direction = 0,
+                    }
+
+                };
+                Grid.SetRow(textBlock, i);
+                Grids[grid_index].Children.Add(textBlock);
+
             }
-
-
+            ParentGrids[grid_index].Children.Add(Emmission);
+            ParentGrids[grid_index].Children.Add(Grids[grid_index]);
            
+            TranslateTransform transform = new TranslateTransform();
+            ParentGrids[grid_index].RenderTransform = transform;
+            gridTransforms[grid_index] = transform;
 
-            }
+            UserControl wrapper = new UserControl
+            {
+                Content = ParentGrids[grid_index],
+                Width = ParentGrids[grid_index].Width,
+                Height = ParentGrids[grid_index].Height,
+            };
 
+            return wrapper;
+        }
 
+        int count = 2;
         
 
-    }
+        void OnMainGrid()
+        {
+            UserControl grid0 = GenGrid(0,3);
+            Grid.SetColumn(grid0, 0);
+            Main.Children.Add(grid0);
+            Oscilation(grid0, 3);
+
+            UserControl grid1 = GenGrid(1, 6);
+            grid1.Margin = new Thickness(0, 0, 0, offset);
+            Grid.SetColumn(grid1, 1);
+            Main.Children.Add(grid1);
+
+            UserControl grid3 = GenGrid(1, 6);
+            grid3.Margin = new Thickness(0, 0, 0, offset);
+            Grid.SetColumn(grid3, 3);
+            Main.Children.Add(grid3);
+
+            UserControl grid4 = GenGrid(2, 10);
+            grid4.Margin = new Thickness(0, 0, 0, offset);
+            Grid.SetColumn(grid4, 4);
+            Main.Children.Add(grid4);
+
+            UserControl grid6 = GenGrid(1, 6);
+            grid6.Margin = new Thickness(0, 0, 0, offset);
+            Grid.SetColumn(grid6, 6);
+            Main.Children.Add(grid6);
+
+            UserControl grid7 = GenGrid(2, 10);
+            grid7.Margin = new Thickness(0, 0, 0, offset);
+            Grid.SetColumn(grid7, 7);
+            Main.Children.Add(grid7);
+
+        }
 
     }
+}
